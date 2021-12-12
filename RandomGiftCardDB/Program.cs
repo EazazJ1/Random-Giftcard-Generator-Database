e7ccf8629cc8025493c8baa949aa251a17df62a8
Eazaz Jakda <Ejakda2584@conestogac.on.ca>
2021-12-12 13:06:48 -0500

    Added input validation for the menu choice in Main.

--- RandomGiftCardDB/Program.cs
+++ RandomGiftCardDB/Program.cs
@@ -33,42 +33,52 @@ namespace RandomGiftCardDB
                     myFunctions.menu();
 
                     string input = Console.ReadLine();
-                    int choice = Convert.ToInt32(input);
-
-                    switch (choice)
+                    int test = 0;
+                    bool result = int.TryParse(input, out test);
+                    if(!result)
                     {
-                        case 1:
-                            Console.Clear();
-                            myFunctions.AddToRandomTable(myConn, randomCards);
-                            break;
-                        case 2:
-                            Console.Clear();
-                            myFunctions.getRandomListFromDb(myConn);
-                            break;
-                        case 3:
-                            Console.Clear();
-                            myFunctions.getAverageAmount(myConn);
-                            //int average = myFunctions.getAverageAmount(myConn);
-                            //Console.WriteLine("{0}", average);
-                            break;
-                        case 4:
-                            Console.Clear();
-                            int total = myFunctions.getTotalAmount(myConn);
-                            Console.WriteLine("\nTotal Amount: ${0}", total);
-                            break;
-                        case 5:
-                            Console.Clear();
-                            myFunctions.getHighLowAmount(myConn);
-                            break;
-                        case 6:
-                            Console.WriteLine("Thank You for using the application!");
-                            killswitch = 1;
-                            break;
-                        default:
-                            killswitch = 1;
-                            break;
+                        continue;
                     }
 
+                    int choice = Convert.ToInt32(input);
+                    
+                    if (choice > 0 && choice < 7)
+                    {
+
+                        switch (choice)
+                        {
+                            case 1:
+                                Console.Clear();
+                                myFunctions.AddToRandomTable(myConn, randomCards);
+                                break;
+                            case 2:
+                                Console.Clear();
+                                myFunctions.getRandomListFromDb(myConn);
+                                break;
+                            case 3:
+                                Console.Clear();
+                                myFunctions.getAverageAmount(myConn);
+                                break;
+                            case 4:
+                                Console.Clear();
+                                int total = myFunctions.getTotalAmount(myConn);
+                                Console.WriteLine("\nTotal Amount: ${0}", total);
+                                break;
+                            case 5:
+                                Console.Clear();
+                                myFunctions.getHighLowAmount(myConn);
+                                break;
+                            case 6:
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("\nThank You for using the Random Gift Card Genrator application!");
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                killswitch = 1;
+                                break;
+                            default:
+                                killswitch = 1;
+                                break;
+                        }
+                    }
                 }
             }
             catch (Exception ex)
